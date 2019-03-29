6aefc9b2006354d982a8532918215f570c2954aa
Jake-Foster <35465022+Jake-Foster@users.noreply.github.com>
2019-03-28 20:36:55 -0400

    Making this opening window pretty as hell

--- LibraryDeskApp/Model/LibraryBook.cs
+++ LibraryDeskApp/Model/LibraryBook.cs
@@ -9,7 +9,7 @@ namespace LibraryBookDeskApp.Model
 {
     class LibraryBook
     {
-        public long Id                          { get; set; }
+        public long Id                         { get; set; }
         public string Title                    { get; set; }
         public string Author                   { get; set; }
         public string Publisher                { get; set; }
